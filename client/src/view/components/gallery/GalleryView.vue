d8b74437d0d8d575e2a31e2605ab4552efa235ee
Vladimir Minkin <Vladimir.Minkin@massive.co>
2018-01-28 21:07:51 +0100

    GalleryViewItem update, view component consume only raw value from computed properties

--- client/src/view/components/gallery/GalleryView.vue
+++ client/src/view/components/gallery/GalleryView.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="gallery-view" v-if="view">
-    <GalleryPageItem v-for="item in view.items" :url="item.link" :key="item.uri"/>
+  <div class="gallery-view" v-if="available">
+    <GalleryViewItem v-for="item in items" :url="item.link" :key="item.uri"/>
   </div>
 </template>
 
 <script>
 
 import { createNamespacedHelpers } from 'vuex'
-import GalleryPageItem from '@/view/components/gallery/GalleryViewItem'
+import GalleryViewItem from '@/view/components/gallery/GalleryViewItem'
 import { GALLERY_STORE_NAME } from '@/consts/StoreNames'
 
 const { mapState } = createNamespacedHelpers(GALLERY_STORE_NAME)
@@ -16,15 +16,13 @@ export default
 {
   name: 'GalleryView',
   components: {
-    GalleryPageItem
+    GalleryViewItem
   },
   computed: {
-    ...mapState([
-      'view'
-    ])
-  },
-  beforeUpdate () {
-    console.log('beforeUpdate', this.page)
+    ...mapState({
+      available: state => (state.view != null),
+      items: state => state.view.items
+    })
   }
 }
 </script>
