282e2676c252071117320c1064ea53c7222edf91
Vladimir Minkin <Vladimir.Minkin@massive.co>
2018-03-29 11:43:07 +0200

    Initial value for user name was removed

--- client/src/model/vos/user/UserSettingsVO.js
+++ client/src/model/vos/user/UserSettingsVO.js
@@ -1,7 +1,7 @@
 export default class UserSettingsVO {
   constructor () {
     this._id = 'settings'
-    this.userID = 'invisart'
+    this.userID = ''
     this.accessToken = '820f17fe59cb8fddae47bbe322cad652'
   }
 }
